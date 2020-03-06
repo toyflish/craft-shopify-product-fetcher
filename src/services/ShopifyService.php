f02c3a97b5af5643a67d76c3e66d59202a650188
Kai Rautenberg <mail@kairautenberg.de>
2020-03-06 14:14:28 +0100

    fixed limit 250, truncate select option value

--- src/services/ShopifyService.php
+++ src/services/ShopifyService.php
@@ -19,7 +19,7 @@ class ShopifyService extends Component
      * @param array $options
      * @return bool
      */
-    public function getProducts($options = array())
+    public function getProducts($options = array('limit'=> 250))
     {
         $settings = \shopify\Shopify::getInstance()->getSettings();
 
